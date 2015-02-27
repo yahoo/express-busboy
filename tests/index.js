7973e1d23e145b78f232235267550de9a3b12eb3
Dav Glass <davglass@gmail.com>
2015-02-27 12:41:32 -0600

    updated copyright

--- tests/index.js
+++ tests/index.js
@@ -7,22 +7,7 @@ var vows = require('vows'),
     assert = require('assert'),
     bb = require('../'),
     express = require('express'),
-    fs = require('fs'),
-    request = require('request'),
-    portfinder = require('portfinder');
-
-var port;
-var app = express();
-var base = 'http://127.0.0.1:';
-var setup = function(app) {
-    app._server = app.listen(port);
-    app.all('/', function(req, res) {
-        res.send({
-            body: req.body,
-            files: req.files
-        });
-    });
-};
+    app = express();
 
 var tests = {
     'should export': {
